40c973bb123b90d6cddf83511595760332f76290
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-01-10 18:53:00 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [code, setCode] = useState('');
+  useEffect(() => {
+    fetch('code.txt')
+      .then((res) => res.text())
+      .then((text) => setCode(text));
+  }, []);
+  return (
+    <div id="container">
+      <div id="source">hello world</div>
+    </div>
+  );
 }
 
 export default App;
